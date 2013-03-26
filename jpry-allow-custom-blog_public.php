8fba784aa238843ff6a5f5b3f934698dce8d065a
Jeremy Pry <jeremy.pry@gmail.com>
2013-03-26 17:34:34 -0300

    Added main code for the plugin

--- jpry-allow-custom-blog_public.php
+++ jpry-allow-custom-blog_public.php
@@ -1,1 +1,16 @@
 <?php
+/**
+ * Plugin Name: Allow custom blog_public on WPE Staging
+ * Description: This plugin allows you to set your own blog_public option on a WP Engine Staging site. <strong>Use with care, as this could allow your site to be indexed by Search Engines</strong>
+ * Version: 1.0
+ * Author: Jeremy Pry <jeremy@wpengine.com>
+ * Author URI: http://jeremypry.com/
+ * License: GPL3
+ */
+
+// The is_wpe_snapshot() determines whether we're on the staging site or production site. 
+if ( function_exists( 'is_wpe_snapshot' ) ) {
+     if ( is_wpe_snapshot() ) {
+         remove_filter( 'pre_option_blog_public', 'wpe_filter_privacy_option' );
+     }
+}
